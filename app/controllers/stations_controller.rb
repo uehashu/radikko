169e07fcca84b8a1b8fd87bc608afbf6f1d9f1cc
uehashu <uehashu@gmail.com>
2015-06-14 09:06:02 +0900

    add Navigation sidebar to station show page
    放送局ごとの番組表に, 曜日によるサイドバーを追加した.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class StationsController < ApplicationController
   before_action :set_item, only: [:show]
   
@@ -6,6 +7,7 @@ class StationsController < ApplicationController
   end
 
   def show
+    
   end
   
   
@@ -13,9 +15,20 @@ class StationsController < ApplicationController
   private
 
   def set_item
-    @station_id = Station.find(params[:id]).station_id
-    @area_id = Station.find(params[:id]).area_id
-    @station_name = Station.find(params[:id]).station_name
-    @programs = Program.where(station_id: @station_id)
+    @today = Date.today
+    @week = Array.new(7);
+    for i in 0..6 do
+      @week[i] = (@today - @today.cwday + i + 1)
+    end
+    
+    @station = Station.find(params[:id])
+    @station_id = @station.station_id
+    @area_id = @station.area_id
+    @station_name = @station.station_name
+    @programs = Program.where(station_id: @station_id).where("start_date >= ?", @week[0].to_s)
+    @firstProgram_ids_ofDay = Array.new(7)
+    for i in 0..6 do
+      @firstProgram_ids_ofDay[i] = @programs.find_by("start_date >= ? and start_date < ?", @week[i].to_s, (@week[i]+1).to_s).id
+    end
   end
 end

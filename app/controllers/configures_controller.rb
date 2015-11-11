2030f9e86eb7f95bac77ea432026c8fe5007ddfa
uehashu <uehashu@gmail.com>
2015-11-12 08:24:58 +0900

    add 3 configure items
    自前で rtmpdump と swfextract と ffmpeg 作るのは面倒なので, 当分は外部コマンドとしてこれらを呼び出す.
    そいつらのパスを設定する項目を追加した.

--- app/controllers/configures_controller.rb
+++ app/controllers/configures_controller.rb
@@ -1,6 +1,9 @@
 class ConfiguresController < ApplicationController
   
   def edit
+    @path_rtmpdump = Configure.find_or_create_by(key: "path_rtmpdump").value
+    @path_swfextract = Configure.find_or_create_by(key: "path_swfextract").value
+    @path_ffmpeg = Configure.find_or_create_by(key: "path_ffmpeg").value
     @storedir = Configure.find_or_create_by(key: "storedir").value
   end
   
@@ -8,6 +11,20 @@ class ConfiguresController < ApplicationController
   
   def update
     if params[:commit] == "change"
+
+      # update path_rtmpdump
+      path_rtmpdump_record = Configure.find_or_create_by(key: "path_rtmpdump")
+      path_rtmpdump_record.update_attributes(value: params[:path_rtmpdump])
+
+      # update path_swfextract
+      path_rtmpdump_record = Configure.find_or_create_by(key: "path_swfextract")
+      path_rtmpdump_record.update_attributes(value: params[:path_swfextract])
+
+      # update path_ffmpeg
+      path_rtmpdump_record = Configure.find_or_create_by(key: "path_ffmpeg")
+      path_rtmpdump_record.update_attributes(value: params[:path_ffmpeg])
+      
+      # update storedir
       storedir_record = Configure.find_or_create_by(key: "storedir")
       if params[:storedir].empty?
         @error = "Empty storedir is not allowed."
@@ -18,8 +35,14 @@ class ConfiguresController < ApplicationController
           @error = "Failed to update configure."
         end
       end
+
+      # re-render
+      @path_rtmpdump = Configure.find_or_create_by(key: "path_rtmpdump").value
+      @path_swfextract = Configure.find_or_create_by(key: "path_swfextract").value
+      @path_ffmpeg = Configure.find_or_create_by(key: "path_ffmpeg").value
       @storedir = Configure.find_or_create_by(key: "storedir").value
       render :edit
+
     elsif params[:commit] == "cancel"
       @storedir = Configure.find_or_create_by(key: "storedir").value
       render :edit

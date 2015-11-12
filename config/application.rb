cfcf42e4c1a95118be4bf6b4a0acae0dc4da0bca
uehashu <uehashu@gmail.com>
2015-11-12 20:41:59 +0900

    add autoload_paths
    require するのは面倒(rails 的にも邪道っぽい)なので, autoload_paths で処理することにした.
    ついでに Gemfile.lock をアップデート.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Radikko
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # To autoload lib/ directory
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end

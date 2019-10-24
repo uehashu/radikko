c6176ef91f4599315e53e56655fbb79544fccb4c
uehashu <uehashu@gmail.com>
2019-10-25 05:39:33 +0900

    bugfix for production env

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Radikko
     config.active_record.default_timezone = :local
 
     config.autoload_paths += Dir["#{config.root}/lib"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
   end
 end

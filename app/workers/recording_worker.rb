004844afb07743b76bd252e182b29590ccabbabd
uehashu <uehashu@gmail.com>
2020-12-02 16:40:39 +0900

    modify recording_worker.rb
    保存場所が設定されていないときの挙動がちゃんと修正されていなかった．

--- app/workers/recording_worker.rb
+++ app/workers/recording_worker.rb
@@ -10,8 +10,15 @@ class RecordingWorker
     Rails.logger.info("pulsed")
     recording = Recording.find(recording_id)
 
+    # Configureで保存場所が設定されていて，かつ空白でない場合は，設定された値を使う．
+    # 設定されていない，もしくは設定されていても空白な場合は，デフォルトの値を使う．
+    storedir = '/var/radikko'
+    if Configure.exists?(key: 'storedir') &&
+         !Configure.find_by(key: 'storedir').value.blank?
+      storedir = Configure.find_by(key: 'storedir').value
+    end
+
     recording.update_attribute(:is_recorded, true)
-    storedir = Configure.where(key: "storedir").first.value || "/var/radikko"
     filename_full = storedir + "/" + recording.filename
     ProgramRecorder.record(recording.station_id,
                            recording.recording_second,
